9cc927acfe470e2b86be0820129f54131f9ced85
Sathya Rajesh <sathyarajeshpk@gmail.com>
2026-03-03 18:31:09 +0530

    Fixed Dockerfile content Finalsss2

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
-
-echo "Database URI inside container:"
-echo $SQLALCHEMY_DATABASE_URI
+set -e
 
 echo "Running Superset DB migrations..."
 superset db upgrade
@@ -9,11 +7,12 @@ superset db upgrade
 echo "Initializing Superset..."
 superset init
 
-echo "Starting Superset..."
+echo "Starting Gunicorn..."
 
-gunicorn \
+exec gunicorn \
   --workers 1 \
   --threads 2 \
+  --worker-class gthread \
   --timeout 120 \
   --bind 0.0.0.0:$PORT \
   "superset.app:create_app()"
